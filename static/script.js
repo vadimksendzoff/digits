036b570b2a0f692fd9f65fb5886f3da77c2648a5
vadimks1 <check1project@gmail.com>
2024-08-13 02:02:40 +0300

    digits to html

--- static/script.js
+++ static/script.js
@@ -1,4 +1,13 @@
 
+
+function setDigitsToHtml (digit) {
+
+    digitElement = document.getElementById('dItem');
+    digitElement.textContent = "&nbsp" + digit;
+
+}
+
+
 function getDigits() {
     axios.get('https://ksendzov.org/get_digits').then(function (response) {
         console.log("-----", response.data[0][0])
